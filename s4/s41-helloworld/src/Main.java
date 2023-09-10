01eb57898cd5e4aba5fec7d585fd622298fe3a95
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-10 22:41:36 +0700

    jdkName="1.8" LANGUAGE_LEVEL="JDK_1_5"

--- s4/s41-helloworld/src/Main.java
+++ s4/s41-helloworld/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello, Tim");
+
+    boolean isAlien = false;
+    if (isAlien == false) {
+      System.out.println("It is not an alien!");
+      System.out.println("And I am scared of aliens");
+    }
+
+//    int[] arrayInt = new int[] {0,1,2,3,4,5,6,7,8,9};
+//
+//    for (int i = 0; i < arrayInt.length; i++) {
+//      System.out.print( i==0 ? i : ", " + i);
+//    }
+
   }
 }
